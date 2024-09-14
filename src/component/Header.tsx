be8d73b1e297501e72dac3d4119eec11c2c18b33
nasrullohfajar <nasrullohfajar10@gmail.com>
2024-09-14 15:18:11 +0700

    add: cart icon

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import Cart from "../assets/cart.png";
 
 const Header = () => {
   return (
     <div className="flex h-16 items-center justify-between px-36 shadow">
       <h1 className="text-3xl">BeliOnline</h1>
-      <div>
+      <div className="flex items-center justify-center gap-5">
+        <a href="">
+          <img src={Cart} alt="cart icon" className="h-6" />
+        </a>
+
         <p className="text-lg">John Doe</p>
       </div>
     </div>
