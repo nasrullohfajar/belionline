7795e38f02f2580167421944ab2314c62645a12e
nasrullohfajar <nasrullohfajar10@gmail.com>
2024-09-15 22:10:14 +0700

    add: hero image

--- src/component/layout/HeroImage.tsx
+++ src/component/layout/HeroImage.tsx
@@ -1,8 +1,14 @@
+import image from "../../../public/img/heroimage.jpg";
+
 const HeroImage = () => {
   return (
-    <div className="h-[200px] lg:h-[400px] bg-[#F4F4F4]">
-      <img src="" alt="" />
-    </div>
+    <div
+      className="h-[200px] w-full lg:h-[400px] bg-cover bg-no-repeat overflow-hidden"
+      style={{
+        backgroundImage: `url(${image})`,
+        backgroundPosition: "50% 40%",
+      }}
+    ></div>
   );
 };
 
