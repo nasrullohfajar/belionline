61d7e588afd5a879b576bbf4ca448f25d6db496e
nasrullohfajar <nasrullohfajar10@gmail.com>
2024-09-15 11:07:43 +0700

    fix: icon

--- src/component/layout/Header.tsx
+++ src/component/layout/Header.tsx
@@ -1,4 +1,4 @@
-import Cart from "../../assets/cart.png";
+import { IoCart } from "react-icons/io5";
 
 const cart = ["item1", "item2", "item3"];
 // const cart = [];
@@ -9,10 +9,11 @@ const Header = () => {
       <h1 className="text-lg">TreasureCart</h1>
       <div className="flex items-center justify-center gap-4">
         <a href="" className="relative">
-          <img src={Cart} alt="cart icon" className="h-4" />
+          {/* <img src={Cart} alt="cart icon" className="h-4" /> */}
+          <IoCart size={18} />
           {cart.length > 0 ? (
             <div className="absolute flex items-center justify-center bottom-4 left-4 bg-[#373737] h-3 w-3 rounded-full">
-              <p className="text-[7px] text-white">{cart.length}</p>
+              <p className="text-[5px] text-white">{cart.length}</p>
             </div>
           ) : (
             <></>
